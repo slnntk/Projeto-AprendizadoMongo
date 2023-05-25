ae5f4bc8760f1112d86aad332e66b1e1b91d7a0b
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-25 13:59:28 -0300

    Delete user by id

--- src/main/java/com/cassio/workshopmongo/resources/UserResources.java
+++ src/main/java/com/cassio/workshopmongo/resources/UserResources.java
@@ -48,4 +48,10 @@ public class UserResources {
         return ResponseEntity.created(uri).build();
     }
 
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build(); // 204/
+    }
+
 }
